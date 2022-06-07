d2ed64fabed544dce63b84bf825e48c17c2fb6d8
Lil-Konstant <84564075+Lil-Konstant@users.noreply.github.com>
2022-06-07 13:27:51 +1000

    Add files via upload

--- include/Client.h
+++ include/Client.h
@@ -1,56 +1,84 @@
 #pragma once
 
 #include <unordered_map>
+#include <chrono>
 #include <glm/mat4x4.hpp>
-#include <RakPeerInterface.h>
 #include "Application.h"
 #include "GameObject.h"
 
+/// <summary>
+/// Client is one of the main classes implemented in the NetworkingModule, containing all the logic for
+/// initialising and managing a networked connection to the Server.exe application, and is the 
+/// class from which you should derive your GameClient for whatever game you're making to make 
+/// use of the module properly. The class maintains a list of local GameObjects (the first element
+/// of which is assumed to be the player object), as well as a map of other-client GameObjects, 
+/// all of which are updated and drawn in the corresponding Application override functions. On 
+/// startup() the client will establish a RakNet managed connection to the game server using 
+/// the member PORT and IP variables (which can be overriden in your derived GameClient), and
+/// will then loop through all pending server packets every frame and address them appropriately.
+/// The class has event-like functions that are called in response to receiving specific packts,
+/// and also has pre-made functions for sending packets to the server (like requesting to start
+/// the game or requesting to spawn an object for all other players). Client's are configured to
+/// automatically send their GameObject data whenever a change in velocity is detected, and Client's
+/// use dead reckoning to update other-client GameObjects in between client data packets.
+/// class).
+/// </summary>
 class Client : public aie::Application {
 public:
 
-	Client();
-	virtual ~Client();
+	Client() { }
+	virtual ~Client() { }
 
+	// Standard application functions, can be overriden in derived classes but require base class call-backs somewhere in code
 	virtual bool startup();
 	virtual void shutdown();
-
 	virtual void update(float deltaTime);
 	virtual void draw();
 
+	// Getters
 	GameObject* getClientGameObject() { return m_localGameObjects[0]; };
 	std::vector<GameObject*> getLocalGameObjects() { return m_localGameObjects; }
 
 protected:
 	
-	virtual void CreateGameObject();
-	virtual void setStartPosition();
-	virtual void endGame();
-
-	void initialiseClientConnection();
-	void handleNetworkMessages();
-	
-	// Receiving packet logic
-	virtual void onSpawnObject(glm::vec3 spawnPos, glm::vec3 spawnVel) { }
-	virtual void onStartGame() { } // start game event, override in your game code
-	void onSetClientIDPacket(RakNet::Packet* packet);
-	void onReceivedClientDataPacket(RakNet::Packet* packet);
-	void onDespawnObject(RakNet::Packet* packet);
-	// Sending packet logic
-	void sendGameObject(GameObject* gameObject);
-	void sendSpawnObjectPacket(glm::vec3 spawnPos, glm::vec3 spawnVel);
-	void sendStartGamePacket();
-	void sendDespawnObjectPacket(int id);
-
+	// The network information for the client to connect to
 	RakNet::RakPeerInterface* m_pPeerInterface;
-	const char* IP = "25.59.116.52";
+	const char* IP = "127.0.0.1"; // Reassign IP member variable with whatever server IP you wish to run on in your game
 	const unsigned short PORT = 5456;
 
+	// List of local client objects, and hash map of all other objects in the game
 	std::vector<GameObject*> m_localGameObjects;
-	glm::vec3 m_oldVelocity;
 	std::unordered_map<int, GameObject*> m_otherClientGameObjects;
+	glm::vec3 m_oldVelocity; // Used to changed for new client velocities
 
+	// Camera variables
 	glm::mat4	m_viewMatrix;
 	glm::mat4	m_projectionMatrix;
-	bool m_gameOver = false;
+
+	// Server ping variables
+	std::chrono::steady_clock::time_point m_pingStart;
+	std::chrono::steady_clock::time_point m_pingEnd;
+
+	// Standard network handling
+	void initialiseClientConnection();
+	void handleNetworkMessages();
+
+	// Receiving packet logic
+	virtual void onStartGame() { } // Start game event called by the server, override in your game code
+	void onSetClientIDPacket(RakNet::Packet* packet); // Called when this client connects to the server, sets ID and start position
+	void onReceivedClientDataPacket(RakNet::Packet* packet); // Called when new/updated other-client data is received
+	void onDespawnObject(RakNet::Packet* packet); // Despawn object event called by the server
+	void onPingResponse();
+
+	// Sending packet logic
+	void sendGameObject(GameObject* gameObject); // Sends this game object data to other clients
+	void sendStartGamePacket(); // Requests to start the game
+	void sendSpawnObjectPacket(glm::vec3 spawnPos, glm::vec3 spawnVel); // Request to spawn server-side object
+	void sendDespawnObjectPacket(int id); // Requests to despawn a specific object for all clients and server
+	void sendPingQueryPacket(); // Used to query the ping latency of the server
+
+	// Utility functions, override with your own game logic
+	virtual void createGameObject();
+	virtual void setStartPosition();
+	virtual void endGame(bool hasWon);
 };
--- include/GameObject.h
+++ include/GameObject.h
@@ -4,27 +4,41 @@
 #include <glm/vec4.hpp>
 #include <RakPeerInterface.h>
 
+/// <summary>
+/// GameObject is a class used to represent the physical objects in the game scene of
+/// all clients. The class contains, by default, position, velocity, colour, radius and
+/// client ID, all of which it is configured to read in from the server and write out to
+/// the server when requested by either this client or the server. The class is setup to be
+/// easily derivable, containing virtual draw and update function that can be overriden
+/// with your own drawing and updating logic for whatever GameObject you wish to make, as
+/// well as virtual read and write functions that can be overriden if the derived GameObject
+/// you wish to make needs to transmit more data than what it does by default (e.g. health
+/// or attack).
+/// </summary>
 class GameObject
 {
 public:
+	// Transmitted data
 	glm::vec3 position;
 	glm::vec3 velocity;
 	glm::vec4 colour;
 	float radius;
 	int ID;
 	
+	// Local data
 	float lifetime;
-	glm::vec3 localPosition; // Stored position the client thinks this object is at
+	glm::vec3 localPosition; // Local position the client thinks this object is at
 
 	GameObject();
-
-	// Override these functions in your derived GameObject to use your own drawing or update logic
-	virtual void draw() { aie::Gizmos::addSphere(localPosition, radius, 32, 32, colour); };
-	virtual void update(float deltaTime);
-
-	void Write(RakNet::RakPeerInterface* pPeerInterface, const RakNet::SystemAddress& address, bool broadcast);
-	void Read(RakNet::Packet* packet);
-
-	void reconcilePosition(glm::vec3 currentPosition, glm::vec3 goalPosition, float percentage);
+	~GameObject() { }
 	static glm::vec4 GetColour(int id);
+
+	// Override these functions in your derived GameObject to use your own drawing, updating, or lerping logic
+	virtual void draw() { aie::Gizmos::addSphere(localPosition, radius, 8, 8, colour); }; // Draw the game object as a sphere using bootstrap by default
+	virtual void update(float deltaTime);
+	virtual void reconcilePosition(float percentage);
+
+	// Network functions, override if you wish to add extra data to your GameObjects 
+	virtual void Write(RakNet::RakPeerInterface* pPeerInterface, const RakNet::SystemAddress& address, bool broadcast);
+	virtual void Read(RakNet::Packet* packet);
 };
